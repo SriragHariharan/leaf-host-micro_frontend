641072d19a1fbd5470e1a9dbf13302e93abbf9d6
Srirag Hariharan <sriraghariharan108@gmail.com>
2025-03-19 04:14:31 +0530

    Changed backend URL from localhost to the remote server URL.

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,1 +1,2 @@
-export const NOTIFICATION_SERVICE_URL = 'ws://localhost:2005';
+// export const NOTIFICATION_SERVICE_URL = 'ws://localhost:2005';
+export const NOTIFICATION_SERVICE_URL = 'wss://api.leaf.monster/api/v1/notification';
